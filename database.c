7703fa38073eff70ec8f681373e440876e5be9ed
Emily Archey <emilyarchey@gmail.com>
2019-02-20 23:59:18 -0600

    beginning of file i/o

--- database.c
+++ database.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct food {
+typedef struct Food {
     char name[200];
     int NDB_number;
     char manufacturer[100];
@@ -15,9 +15,24 @@ typedef struct food {
     int sugars;
     int calories;
     int sodium;
+
+    struct Food *next;
 };
 
-int main (void) {
+int main (int argc, char *argv[]) {
+    /*if (argc != 4) {
+        printf("Usage: productsfile nutrientsfile servingsizefile");
+    }*/
+
+    FILE *nutrients;
+    FILE *products;
+    FILE *serving_size;
+
+    products = fopen(argv[1], "r");
+    struct Food *head = NULL;
+    head = (struct Food*)malloc(sizeof(struct Food));
+    string temp[100];
+    fgets(temp, 100, products);
 
     return 0;
 }
