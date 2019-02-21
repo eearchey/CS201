f86c2f416cddb564db28e05a179d0c3251e39f82
Emily Archey <emilyarchey@gmail.com>
2019-02-20 23:14:55 -0600

    moved struct to database from main

---
+++ database.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct food {
+    char name[200];
+    int NDB_number;
+    char manufacturer[100];
+    int servingSize;
+    char servingSizeUnit[50];
+
+    //nutrients
+    int protein;
+    int fat;
+    int sugars;
+    int calories;
+    int sodium;
+};
--- main.c
+++ main.c
@@ -2,21 +2,6 @@
 #include <string.h>
 #include <math.h>
 
-typedef struct food {
-    char name[200];
-    int NDB_number;
-    char manufacturer[100];
-    int servingSize;
-    char servingSizeUnit[50];
-
-    //nutrients
-    int protein;
-    int fat;
-    int sugars;
-    int calories;
-    int sodium;
-};
-
 
 int main (void) {
 
