9ae87463d0f16818c37eec4a15469d9afcd850c3
Emily Archey <emilyarchey@gmail.com>
2019-02-28 14:12:32 -0600

    im stupid, isn't a header

--- database.c
+++ database.c
@@ -1,10 +1,10 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-#ifndef DATABASE_H
-#define DATABASE_H
 
 typedef struct Food {
     char name[200];
