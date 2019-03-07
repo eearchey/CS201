1bb8f422139837a4491f7d1c24f143f1d5ae37e6
Emily Archey <emilyarchey@gmail.com>
2019-03-07 17:21:52 -0600

    fixed food qualities

--- database.c
+++ database.c
@@ -45,43 +45,15 @@ Food *addData(char temp[], char tempy[]) {
 
 
 void createList(char productsFile[], char serving_sizeFile[], char nutrientsFile[]) {
-    FILE *products;
-    FILE *serving_size;
-    FILE *nutrients;
+    FILE *food_database;
 
-    products = fopen(productsFile, "r");
-    serving_size = fopen(serving_sizeFile, "r");
-    Food *head = NULL;
+    foods = fopen(food_database, "r");
+    Food *root = NULL;
     Food *cur = NULL;
-    head = (struct Food*)malloc(sizeof(struct Food));
-    //cur = (struct Food*)malloc(sizeof(struct Food));
+    root = (struct Food*)malloc(sizeof(struct Food));
     head->next = NULL;
 
-    //getting the titles of the columns out of the way
-    //you can do products and serving sizes at the same time because they line up
-    char temp[1000];
-    char tempy[1000];
-    fgets(temp, 200, products);
-    fgets(tempy, 200, serving_size);
-
-    fgets(temp, 1000, products);
-    fgets(tempy, 1000, serving_size);
-    head = addData(temp, tempy);
-
-    //printf("%d number \n %s name \n %s manufacturer \n", head->NDB_number, head->name, head->manufacturer);
-    //printf("%d serving size \n %s serving size unit \n", head->householdServingSize, head->servingSizeUnit);
     
-    while (fgets(temp, 1000, products) != NULL) {
-        fgets(tempy, 1000, serving_size);
-        cur = addData(temp, tempy);
-        cur->next = head;
-        head = cur;
-        printf("%d number \n %s name \n %s manufacturer \n", head->NDB_number, head->name, head->manufacturer);
-        printf("%d serving size \n %s serving size unit \n", head->householdServingSize, head->servingSizeUnit);
-    }
-
-    fclose(products);
-    fclose(serving_size);
 
     return;
 }
