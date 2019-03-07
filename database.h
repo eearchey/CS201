731356a3a1c1370c8e15da578340efbe5aa04ec0
Emily Archey <emilyarchey@gmail.com>
2019-03-07 17:21:36 -0600

    fixed qualities of food

--- database.h
+++ database.h
@@ -1,21 +1,23 @@
+#include <stdbool.h>
 #ifndef DATABASE_H
 #define DATABASE_H
 
 typedef struct Food {
+    int ID;
     char name[200];
-    int NDB_number;
-    char manufacturer[100];
-    int householdServingSize;
-    char servingSizeUnit[50];
+    char manufacturer[200];
+    float calories;
+    float carbs;
+    float fat;
+    float protein;
+    float serving_size;
+    bool isGrams;
+    float house_serving_size;
+    char serving_size_unit[100];
 
-    //nutrients
-    int protein;
-    int fat;
-    int sugars;
-    int calories;
-    int sodium;
 
-    struct Food *next;
+    struct Food *leftChild;
+    struct Food *rightChild;
 } Food;
 
 #endif
