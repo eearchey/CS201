6815caf82856ec58a35302b32e4f3f3e2536e885
Emily Archey <emilyarchey@gmail.com>
2019-03-14 00:23:18 -0400

    added options

--- main.c
+++ main.c
@@ -12,7 +12,25 @@ int main (int argc, char *argv[]) {
     char fileName[100];
     strcpy(fileName, argv[1]);
     createTree(fileName);
-    
+
+    printf("Welcome to your diary!\n");
+    printf("Options:\nView diary\nAdd entry\nUpdate entry\nDelete entry\n");
+    printf("Enter your choice: ");
+    char choice[50];
+    scanf("%s", choice);
+
+    if (strcasestr(choice, "view") != NULL) {
+        printf("You've chosen to view!\n");
+    }
+    else if (strcasestr(choice, "add") != NULL) {
+        printf("You've chosen to add!\n");
+    }
+    else if (strcasestr(choice, "update") != NULL) {
+        printf("You've chosen to update!\n");
+    }
+    else if (strcasestr(choice, "delete") != NULL) {
+        printf("You've chosen to delete!\n");
+    }
 
     return 0;
 }
